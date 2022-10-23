da74b500933596a59181188a69f9c2e8566106b7
Sahrija <114372392+Sahrija@users.noreply.github.com>
2022-10-23 23:46:47 +0700

    change
    just change

---
+++ praktikum 3/print.py
@@ -0,0 +1,8 @@
+alamat = "Kp. Sawah, Jalan Akses Ui Gg. Realita No.1, RT.4/RW.1, Srengseng Sawah, Jagakarsa (Kos an tanteumy no 4), KOTA JAKARTA SELATAN, JAGAKARSA, DKI JAKARTA, ID, 12640"
+nim = "01102222250"
+kelas = "TI04"
+
+print("Nim: " + nim)
+print("Nama: Indah Agustin")
+print("Kelas: " + kelas)
+print("Alamat: " + alamat)
